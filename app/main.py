ae4c54ef1fb4c7a365d5c41836b4dfadb4492a0b
scanulef-resdev <srelsoporte@gmail.com>
2025-10-20 17:33:57 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 ¡Hola desde Flask en Docker (probando cambios2222)!"
+    return "¡Hola desde Flask en Docker!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
